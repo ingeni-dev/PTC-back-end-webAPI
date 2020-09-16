cc34ad9a0599381c0eae285a8fb3358c2be50465
parnumat <49821951+parnumat@users.noreply.github.com>
2020-09-16 13:53:57 +0700

    first-deploy-back-dev

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,39 +9,45 @@ using PTCwebApi.Models.AuthenticateModels;
 using PTCwebApi.Models.RequestModels;
 using PTCwebApi.Security;
 
-namespace PTCwebApi.Controllers {
-    [Route ("[controller]")]
+namespace PTCwebApi.Controllers
+{
+    [Route("[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase {
+    public class UsersController : ControllerBase
+    {
         private readonly IMapper _mapper;
         private readonly IJwtGenerator _jwtGenerator;
-        private object _results = null;
-        public UsersController (IMapper mapper, IJwtGenerator jwtGenerator) {
+        public UsersController(IMapper mapper, IJwtGenerator jwtGenerator)
+        {
             _jwtGenerator = jwtGenerator;
             _mapper = mapper;
         }
 
-        [HttpPost ("login")]
-        public ActionResult<AuthenticateResponse> Login (UserLogin model) {
-            Boolean status = AuthenticateUsers.AuthenticateUser (model);
-            if (status != false) {
-                var response = new MapUserProfile (_mapper).UserProfileMapped (model);
-                if (response != null) {
-                    var token = _jwtGenerator.generateJwtToken (response, model.Username);
-                    return new AuthenticateResponse (response, token);
+        [HttpPost("login")]
+        public ActionResult<AuthenticateResponse> Login(UserLogin model)
+        {
+            Boolean status = AuthenticateUsers.AuthenticateUser(model);
+            if (status != false)
+            {
+                var response = new MapUserProfile(_mapper).UserProfileMapped(model);
+                if (response != null)
+                {
+                    var token = _jwtGenerator.generateJwtToken(response, model.Username);
+                    return new AuthenticateResponse(response, token);
                 }
-                return Unauthorized (new { message = "This user doesn't register in the system yet" });
+                return Unauthorized(new { message = "This user doesn't register in the system yet" });
             }
-            return Unauthorized (new { message = "Username or password is incorrect" });
+            return Unauthorized(new { message = "Username or password is incorrect" });
         }
 
         [Authorize]
-        [HttpPost ("getProflie")]
-        public IActionResult GetAllProfile (UserRequestUserProfile model) {
-            var userProfile = _jwtGenerator.DecodeToken (model.token);
-            return Ok (userProfile);
+        [HttpPost("getProflie")]
+        public IActionResult GetAllProfile(UserRequestUserProfile model)
+        {
+            var userProfile = _jwtGenerator.DecodeToken(model.token);
+            return Ok(userProfile);
         }
 
-       
+
     }
 }
