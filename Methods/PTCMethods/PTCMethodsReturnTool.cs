12cd4ca2380315cfcd62808aa9a1abaa35ea7945
parnumat <49821951+parnumat@users.noreply.github.com>
2020-10-12 18:03:35 +0700

    commit-dummy

--- Methods/PTCMethods/PTCMethodsReturnTool.cs
+++ Methods/PTCMethods/PTCMethodsReturnTool.cs
@@ -30,7 +30,7 @@ namespace PTCwebApi.Methods.PTCMethods
             if (model.token != null)
             {
                 UserProfile userProfile = _jwtGenerator.DecodeToken(model.token);
-                var query = $"SELECT * FROM KPDBA.PTC_JS_PLAN_DETAIL WHERE RETURN_DATE IS NULL AND WITHD_USER_ID = '{userProfile.userID}'";
+                var query = $"SELECT * FROM KPDBA.PTC_JS_PLAN_DETAIL WHERE RETURN_DATE IS NULL AND RETURN_USER_ID IS NULL AND WITHD_USER_ID = '{userProfile.userID}'";
                 var result = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, query);
                 // return result;
                 if ((result as List<object>).Count != 0)
@@ -87,36 +87,63 @@ namespace PTCwebApi.Methods.PTCMethods
                         decimal LocValid = (resultCheckLoc as List<dynamic>)[0].COUN;
                         if (LocValid == 1)
                         {
-                            var query = $"SELECT SD.LOC_ID, LOC.LOC_DETAIL, SD.QTY FROM (SELECT SD.WAREHOUSE_ID, SD.LOC_ID, SUM (SD.QTY) QTY FROM KPDBA.PTC_STOCK_DETAIL SD WHERE SD.WAREHOUSE_ID = '{model.warehouseID}' AND SD.PTC_ID = '{model.ptcID}' GROUP BY SD.WAREHOUSE_ID, SD.LOC_ID HAVING SUM (SD.QTY) < 0) SD JOIN (SELECT WAREHOUSE_ID, LOC_ID, LOC_DETAIL FROM KPDBA.LOCATION_PTC) LOC ON (SD.WAREHOUSE_ID = LOC.WAREHOUSE_ID AND SD.LOC_ID = LOC.LOC_ID)";
-                            var resultt = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, query);
-                            decimal count = (resultt as List<object>).Count;
-                            if (count != 0)
+                            var queryQTY = $"SELECT SUM(QTY) QTY FROM KPDBA.PTC_STOCK_DETAIL WHERE WAREHOUSE_ID = '{model.warehouseID}' AND PTC_ID = '{model.ptcID}'";
+                            var resultQTY = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, queryQTY);
+                            var countQty = (resultQTY as List<dynamic>)[0].QTY;
+                            if (countQty == 0)
                             {
-                                var results = _mapper.Map<IEnumerable<GetLoc>>(resultt);
-                                var dataLoc = results.ElementAt(0);
+                                var query = $"SELECT SD.LOC_ID, LOC.LOC_DETAIL, SD.QTY FROM (SELECT SD.WAREHOUSE_ID, SD.LOC_ID, SUM (SD.QTY) QTY FROM KPDBA.PTC_STOCK_DETAIL SD WHERE SD.WAREHOUSE_ID = '{model.warehouseID}' AND SD.PTC_ID = '{model.ptcID}' GROUP BY SD.WAREHOUSE_ID, SD.LOC_ID HAVING SUM (SD.QTY) < 0) SD JOIN (SELECT WAREHOUSE_ID, LOC_ID, LOC_DETAIL FROM KPDBA.LOCATION_PTC) LOC ON (SD.WAREHOUSE_ID = LOC.WAREHOUSE_ID AND SD.LOC_ID = LOC.LOC_ID)";
+                                var resultt = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, query);
+                                decimal count = (resultt as List<object>).Count;
+                                if (count != 0)
+                                {
+                                    var results = _mapper.Map<IEnumerable<GetLoc>>(resultt);
+                                    var dataLoc = results.ElementAt(0);
+                                    var queryCompID = $"SELECT COMP_ID COMP FROM KPDBA.WAREHOUSE WHERE WAREHOUSE_ID ='{model.warehouseID}'";
+                                    var resultCompID = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, queryCompID);
+                                    var compID = (resultCompID as List<dynamic>)[0].COMP;
+                                    var tranSEQ = 1;
+                                    var QTY = "1";
+                                    var S_STATUS = 'T';
+                                    var tranType = "3"; // โอนย้ายออก
+                                    var locID = dataLoc.LOC_ID; // old loc
+                                    string tran_id = await new StoreConnectionMethod(_mapper).PtcGetTranID(compID: model.warehouseID, tranType: compID);
+
+                                    var tranDate = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss", new CultureInfo("en-US"));
+                                    var insertOutQuery = $"INSERT INTO KPDBA.PTC_STOCK_DETAIL (TRAN_ID, TRAN_SEQ, TRAN_TYPE, TRAN_DATE,PTC_ID, QTY, COMP_ID, WAREHOUSE_ID, LOC_ID, STATUS, CR_DATE, CR_ORG_ID, CR_USER_ID) VALUES ('{tran_id}', TO_NUMBER('{tranSEQ}'), TO_NUMBER('{tranType}'), TO_DATE('{tranDate}', 'dd/mm/yyyy hh24:mi:ss'),'{model.ptcID}', TO_NUMBER('{QTY}'), TO_CHAR('{compID}'),'{model.warehouseID}','{locID}', TO_CHAR('{S_STATUS}'), SYSDATE, '{userProfile.org}', '{userProfile.userID}')";
+                                    var resultOutInsert = await new DataContext().InsertResultDapperAsync(DataBaseHostEnum.KPR, insertOutQuery);
+
+                                    string DateNow = DateTime.Today.ToString("dd/MM/yyyy", new CultureInfo("en-US"));
+                                    var querys = $"UPDATE KPDBA.PTC_JS_PLAN_DETAIL SET RETURN_DATE = TO_DATE ('{DateNow}', 'dd/mm/yyyy'), RETURN_USER_ID = TO_CHAR ('{userProfile.userID}') WHERE JOB_ID = '{model.jobID}'";
+                                    var upDateResult = await new DataContext().GetResultDapperAsyncDynamic(DataBaseHostEnum.KPR, querys);
+
+                                }
+                                else
+                                {
+                                    _returnFlag = "1";
+                                    _returnText = "ไม่พบอุปกรณ์ในประวัติการเบิก";
+                                }
+                            }
+                            else if (countQty == null)
+                            {
                                 var queryCompID = $"SELECT COMP_ID COMP FROM KPDBA.WAREHOUSE WHERE WAREHOUSE_ID ='{model.warehouseID}'";
                                 var resultCompID = await new DataContext().GetResultDapperAsyncObject(DataBaseHostEnum.KPR, queryCompID);
                                 var compID = (resultCompID as List<dynamic>)[0].COMP;
                                 var tranSEQ = 1;
                                 var QTY = "1";
                                 var S_STATUS = 'T';
-                                var tranType = "3"; // โอนย้ายออก
-                                var locID = dataLoc.LOC_ID; // old loc
+                                var tranType = "12"; // New tooling
+                                var locID = "$R70"; // old loc
                                 string tran_id = await new StoreConnectionMethod(_mapper).PtcGetTranID(compID: model.warehouseID, tranType: compID);
 
                                 var tranDate = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss", new CultureInfo("en-US"));
                                 var insertOutQuery = $"INSERT INTO KPDBA.PTC_STOCK_DETAIL (TRAN_ID, TRAN_SEQ, TRAN_TYPE, TRAN_DATE,PTC_ID, QTY, COMP_ID, WAREHOUSE_ID, LOC_ID, STATUS, CR_DATE, CR_ORG_ID, CR_USER_ID) VALUES ('{tran_id}', TO_NUMBER('{tranSEQ}'), TO_NUMBER('{tranType}'), TO_DATE('{tranDate}', 'dd/mm/yyyy hh24:mi:ss'),'{model.ptcID}', TO_NUMBER('{QTY}'), TO_CHAR('{compID}'),'{model.warehouseID}','{locID}', TO_CHAR('{S_STATUS}'), SYSDATE, '{userProfile.org}', '{userProfile.userID}')";
                                 var resultOutInsert = await new DataContext().InsertResultDapperAsync(DataBaseHostEnum.KPR, insertOutQuery);
-
-                                string DateNow = DateTime.Today.ToString("dd/MM/yyyy", new CultureInfo("en-US"));
-                                var querys = $"UPDATE KPDBA.PTC_JS_PLAN_DETAIL SET RETURN_DATE = TO_DATE ('{DateNow}', 'dd/mm/yyyy'), RETURN_USER_ID = TO_CHAR ('{userProfile.userID}') WHERE JOB_ID = '{model.jobID}'";
-                                var upDateResult = await new DataContext().GetResultDapperAsyncDynamic(DataBaseHostEnum.KPR, querys);
-
                             }
                             else
                             {
                                 _returnFlag = "1";
-                                _returnText = "ไม่พบอุปกรณ์ในประวัติการเบิก";
+                                _returnText = "อุปกรณ์ยังไม่ถูกเบิกออกจากคลัง";
                             }
                         }
                         else
